fd46d5f20551fc43c083a8ff7fc81803e4058d62
EmmanuelZiviello <manuel.ziviello@gmail.com>
2025-01-20 20:20:45 +0100

    Aggiunto redis per uso con render

--- tasks.py
+++ tasks.py
@@ -2,22 +2,38 @@ import os
 import requests
 from dotenv import load_dotenv
 
+import jinja2
+
 load_dotenv()
 
 DOMAIN=os.getenv("MAILGUN_DOMAIN")
+#email html
+template_loader=jinja2.FileSystemLoader("templates")
+template_env=jinja2.Environment(loader=template_loader)
+#email html
 
-def send_simple_message(to,subject,body):#funzione di mailgun
+#funzione html email
+def render_template(template_filename,**context):
+    return template_env.get_template(template_filename).render(**context)
+#funzione html email
+
+def send_simple_message(to,subject,body,html):#funzione di mailgun
     return requests.post(
   		f"https://api.mailgun.net/v3/{DOMAIN}/messages",
   		auth=("api", os.getenv("MAILGUN_API_KEY")),
   		data={"from": f"Emmanuel Ziviello <mailgun@{DOMAIN}>",
   			"to": [to],
   			"subject": subject,
-  			"text": body})
+  			"text": body,
+            "html":html
+        }
+    )#fino ad html è email solo testo
 
 def send_user_registration_email(email,username):
     return send_simple_message(
         email,
         "Successfully signed up!",
-        f"Hi {username}! You have successfully signed up to the Stores REST API!"
+        f"Hi {username}! You have successfully signed up to the Stores REST API!",
+        #fin qui è per email normali senza jinja 2
+        render_template("email/action.html",username=username)
     )
